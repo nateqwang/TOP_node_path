5fda65005ff5c860507536fb86461801391497bc
nateqwang <natewang.en@gmail.com>
2024-08-09 01:09:28 +0800

    Create function that displays books info

--- javascript/organizing_javascript_code/2_PROJECT_library/libraryProject.js
+++ javascript/organizing_javascript_code/2_PROJECT_library/libraryProject.js
@@ -2,7 +2,7 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = this.isRead();
 }
 
 Book.prototype.isRead = function () {
@@ -17,8 +17,32 @@ Book.prototype.info = function() {
     return (`${this.title} by ${this.author}, ${this.pages} pages,` + ` ${this.isRead()}`)
 }
 
+Book.prototype.numOfKeys = 4;
+
 const hobbit = new Book ('The Hobbit', 'Tolkien', 295, false);
 
 const myLibrary = [];
+
+const libraryDisplay = document.querySelector(".book_container");
+
+function displayBooks () {
+    for (currentBook of myLibrary) {
+        const bookCard = document.createElement('div')
+        for (let i = 0; i < currentBook.numOfKeys; i++) {
+            const bookInfo = document.createElement('p');
+            let currentKey = Object.keys(currentBook)[i]
+            bookInfo.setAttribute('class', `${currentKey}`)
+            bookInfo.textContent = `${currentBook[currentKey]}`;
+            bookCard.appendChild(bookInfo);
+        }
+        libraryDisplay.appendChild(bookCard);
+    }
+
+}
+
+myLibrary.push(hobbit);
+
+
+
 
 
