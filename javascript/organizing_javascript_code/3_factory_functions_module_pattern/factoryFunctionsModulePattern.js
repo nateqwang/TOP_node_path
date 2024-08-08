70239075330ad9b2a827c836bd151bc97214fe17
nateqwang <natewang.en@gmail.com>
2024-08-09 03:12:13 +0800

    factoryFunctionsModulePattern

---
+++ javascript/organizing_javascript_code/3_factory_functions_module_pattern/factoryFunctionsModulePattern.js
@@ -0,0 +1,79 @@
+let globalAge = 23; // This is a global variable
+
+// This is a function - and hey, a curly brace indicating a block
+function printAge (age) {
+  var varAge = 34; // This is a function scoped variable
+
+  // This is yet another curly brace, and thus a block
+  if (age > 0) {
+    // This is a block-scoped variable that exists
+    // within its nearest enclosing block, the if's block
+    const constAge = age * 2;
+    console.log(constAge);
+  }
+
+  // ERROR! We tried to access a block scoped variable
+  // not within its scope
+  console.log(constAge);
+}
+
+printAge(globalAge);
+
+// ERROR! We tried to access a function scoped variable
+// outside the function it's defined in
+console.log(varAge);
+
+function makeAdding (firstNumber) {
+    // "first" is scoped within the makeAdding function
+  const first = firstNumber;
+  return function resulting (secondNumber) {
+    // "second" is scoped within the resulting function
+    const second = secondNumber;
+    return first + second;
+  }
+}
+// but we've not seen an example of a "function"
+// being returned, thus far - how do we use it?
+
+const add5 = makeAdding(5);
+console.log(add5(2)) // logs 7
+const User = function (name) {
+  this.name = name;
+  this.discordName = "@" + name;
+}
+// hey, this is a constructor - 
+// then this can be refactored into a factory!
+
+function createUser (name) {
+  const discordName = "@" + name;
+
+  let reputation = 0;
+  const getReputation = () => reputation;
+  const giveReputation = () => reputation++;
+
+  return { name, discordName, getReputation, giveReputation };
+}
+
+const josh = createUser("josh");
+josh.giveReputation();
+josh.giveReputation();
+
+console.log({
+  discordName: josh.discordName,
+  reputation: josh.getReputation()
+});
+// logs { discordName: "@josh", reputation: 2 }
+const calculator = (function () {
+  const add = (a, b) => a + b;
+  const sub = (a, b) => a - b;
+  const mul = (a, b) => a * b;
+  const div = (a, b) => a / b;
+  return { add, sub, mul, div };
+})();
+
+calculator.add(3,5); // 8
+calculator.sub(6,2); // 4
+calculator.mul(14,5534); // 77476
+
+
+
