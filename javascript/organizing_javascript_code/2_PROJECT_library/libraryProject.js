5c0026d0fa4a245b06a2e625cff24630ddad7f28
nateqwang <natewang.en@gmail.com>
2024-08-09 06:12:05 +0800

    Complete javascript for adding books

--- javascript/organizing_javascript_code/2_PROJECT_library/libraryProject.js
+++ javascript/organizing_javascript_code/2_PROJECT_library/libraryProject.js
@@ -26,6 +26,10 @@ const myLibrary = [];
 const libraryDisplay = document.querySelector(".book_container");
 
 function displayBooks () {
+    const allBooks = Array.from(document.querySelectorAll('.book_card'));
+    for (bookNode of allBooks) {
+        bookNode.remove();
+    }
     for (currentBook of myLibrary) {
         const bookCard = document.createElement('div')
         bookCard.setAttribute ('class', 'book_card')
@@ -49,6 +53,11 @@ const dialogForm = document.querySelector("dialog");
 const showDialog = document.querySelector('.add_book');
 const submitForm = document.querySelector('form button');
 const bookForm = document.querySelector('form');
+
+submitForm.addEventListener('click', () => {
+    dialogForm.close();
+})
+
 
 showDialog.addEventListener('click', () => {
     dialogForm.showModal();
