32cea98f1a0434aed25d84e4cefd567ecb6e41ea
nateqwang <natewang.en@gmail.com>
2024-08-09 04:45:53 +0800

    Add game win logic

--- javascript/organizing_javascript_code/4_tic_tac_toe/script.js
+++ javascript/organizing_javascript_code/4_tic_tac_toe/script.js
@@ -16,6 +16,30 @@ const gameLogic = (function () {
             }
         }
     }
+
+    const checkWin = function () {
+        let gbc = gameboard.gameboardArray;
+        if (gbc[0] === gbc[1] && gbc[0] === gbc[2] && gbc[0] !== 2) {
+            return true;
+        } else if (gbc[3] === gbc[4] && gbc[3] === gbc[5] && gbc[3] !== 2) {
+            return true;
+        } else if (gbc[6] === gbc[7] && gbc[6] === gbc[8] && gbc[6] !== 2) {
+            return true;
+        } else if (gbc[0] === gbc[3] && gbc[0] === gbc[6] && gbc[0] !== 2) {
+            return true;
+        } else if (gbc[1] === gbc[4] && gbc[1] === gbc[7] && gbc[1] !== 2) {
+            return true;
+        } else if (gbc[2] === gbc[5] && gbc[2] === gbc[8] && gbc[2] !== 2) {
+            return true;
+        } else if (gbc[0] === gbc[4] && gbc[0] === gbc[8] && gbc[0] !== 2) {
+            return true;
+        } else if (gbc[2] === gbc[4] && gbc[2] === gbc[6] && gbc[2] !== 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     const playRound = function (player,arrayNum) {
         if ((gameboard.gameboardArray[arrayNum] === 2) && arrayNum <= 9) {
             gameboard.gameboardArray[arrayNum] = player;
@@ -25,6 +49,7 @@ const gameLogic = (function () {
     }
     return {
         playGame,
-        playRound
+        playRound,
+        checkWin
     };
 })()
