fa39d91f33698b8c0389ccda68849dba76e04eb4
nateqwang <natewang.en@gmail.com>
2024-08-09 00:20:19 +0800

    Move Book functions to its protoype

---
+++ javascript/organizing_javascript_code/2_PROJECT_library/libraryProject.js
@@ -0,0 +1,22 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.isRead = function () {
+    if (this.read === true) {
+        return ('already read');
+    } else {
+        return ('not read yet');
+    }
+}
+
+Book.prototype.info = function() {
+    return (`${this.title} by ${this.author}, ${this.pages} pages,` + ` ${this.isRead()}`)
+}
+
+const hobbit = new Book ('The Hobbit', 'Tolkien', 295, false);
+
+const myLibrary = [];
