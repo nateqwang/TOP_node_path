69069f2a430941554b1a1df6ae4104b07497e00d
nateqwang <natewang.en@gmail.com>
2024-08-15 09:45:19 +0800

    Create doc file that contains all node elements to edit

--- javascript/async_and_APIs/2_weather_app_project/script.js
+++ javascript/async_and_APIs/2_weather_app_project/script.js
@@ -21,6 +21,32 @@ const globalSetting = {
 
 }
 
+const doc = {
+
+    search: document.querySelector('.text_input'),
+    fcSelectionBtn: document.querySelector('.fc_selection'),
+    address: document.querySelector('.address'),
+
+    todayTemp: document.querySelector('.today_temp'),
+    todayHigh: document.querySelector('.high'),
+    todayLow: document.querySelector('.low'),
+    todayConditionIcon: document.querySelector('.conditions img'),
+    todayConditionDesc: document.querySelector('.today_condition'),
+
+    todaySunrise: document.querySelector('.sunrise'),
+    todaySunset: document.querySelector('.sunset'),
+    todayHumidity: document.querySelector('.humidity'),
+    todayUV: document.querySelector('.uv_index'),
+    todayVisibility: document.querySelector('.visibility'),
+
+    nowTemp: document.querySelector('.temp_now'),
+    nowFeelsLike: document.querySelector('.feelslike_now'),
+    nowConditionIcon: document.querySelector('.now_condition'),
+    nowConditionDesc: document.querySelector('.condition_now_desc'),
+
+    next24: document.querySelector('.next24')
+}
+
 const getWeather = function (location) {
 
     const fetched = fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?key=34A76APDQMZHC36HZLFSTPTZ7&include=days,hours`, {mode:'cors'})
@@ -34,8 +60,6 @@ const getWeather = function (location) {
         return fetched;
 
 }
-
-const bet = getWeather('bethesda');
 
 const processData = function (data) {
 
@@ -77,6 +101,11 @@ const processData = function (data) {
         daysInfo: allDaysInfo
     }
 }
+
+
+
+// const bet = getWeather('bethesda');
+
 
 
 
